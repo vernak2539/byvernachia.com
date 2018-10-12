d5c34491107138d1c94b1169a8206baad0c46078
Alex Vernacchia <alvernacchia@gmail.com>
2018-10-12 12:58:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+publishing to gh-pages
+
+https://gist.github.com/cobyism/4730490
---
+++ index.js
@@ -0,0 +1,33 @@
+import 'normalize.css';
+import './styles.css';
+import TypeIt from 'typeit';
+
+var typeItInstance = new TypeIt('#its-me', {
+  speed: 60,
+  cursorChar: '_'
+});
+
+typeItInstance
+  .type('<span class="header">Hi, I\'m Alex</span>')
+  .pause(600)
+  .options({ speed: 40 })
+  .break()
+  .type('<span class="sub-text">I like long walks on the beach, poetry, and sunsets</span>')
+  .pause(500)
+  .break()
+  .options({ speed: 50 })
+  .type('<span class="sub-text">Oh wait... wrong site</span>')
+  .pause(800)
+  .break()
+  .type('<span class="sub-text">Let\'s try again</span>')
+  .pause(800)
+  .empty()
+  .options({ speed: 60 })
+  .type('<span class="header">Hi, I\'m Alex</span>')
+  .pause(600)
+  .break()
+  .type('<span class="sub-text">I\'m a software engineer</span>')
+  .break()
+  .type('<span class="sub-text">focusing on front-end web development</span>')
+  .pause(500)
+  .type('<span class="sub-text"> #nerd</span>');
